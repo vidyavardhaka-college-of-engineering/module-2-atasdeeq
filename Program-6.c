365329c4c3d53cdd2d87d776839d2aa15f9892b0
atasdeeq <84765801+atasdeeq@users.noreply.github.com>
2021-06-18 09:42:26 +0530

    Done
    1. The variable name should be the same as declared
    2. '++' is an increment operator
    3. Each line must end with a semi colon

--- Program-6.c
+++ Program-6.c
@@ -1,8 +1,6 @@
 //INPUT -3 1 2 3
-//OUTPUT- Total number of even numbers and odd numbers in the array are 1 and 2
-
-//INSERT THE MISSING CODE 
-
+//OUTPUT- Total number of even numbers and odd numbers in the array are 1 and 2 
+#include<stdio.h>
 int main()
 {
     int arr[10];
@@ -13,28 +11,28 @@ int main()
     scanf("%d",&num);
     //printf("Enter %d elements in array: ", num);
         //printf("Enter the elements");
-    for(i=0;i<n;i++)
+    for(i=0;i<num;i++)
     {
-        scanf("%d",&arr[i]);//missing code
+        scanf("%d",&arr[i]);
     }
 
     evennum = 0; // Assuming 0 even numbers
     oddnum  = 0; // Assuming 0 odd numbers
 
-    for(i=0; i<n; i++)
+    for(i=0; i<num; i++)
     {
         /* If the current element of array is evennumber then increment evennumber count */
-        if(arr[i]%2 == 0) //missing code
+        if(arr[i]%2 == 0) 
         {
-            ++;
+           evennum++;
         }
         else
         {
             oddnum++; // increment oddnumber count
         }
 }
-printf("Total number of even numbers and odd numbers in the array are %d and %d",evennum,oddnum  );//missing code
-//printf("Total number of odd numbers in the array : %d\n",   );//missing code
+printf("Total number of even numbers and odd numbers in the array are %d and %d\n",evennum,oddnum);
+//printf("Total number of odd numbers in the array : %d\n",   );
 
 return(0);    
 }
