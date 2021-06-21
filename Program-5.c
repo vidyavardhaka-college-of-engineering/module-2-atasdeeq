6b2e0ffce6ba53760b4dd868f80cbb12b8e568a6
atasdeeq <84765801+atasdeeq@users.noreply.github.com>
2021-06-21 13:26:15 +0530

    Updated

--- Program-5.c
+++ Program-5.c
@@ -12,10 +12,11 @@ int main()
   // printf("Enter %d elements\n", n);
  
    for (i = 0; i < n; i++)
-   {
+   
       scanf("%d", &a[i]);
+    for(i=0; i<n ;i++)
       sum=sum+a[i]; 
-   }
+   
  printf("Sum of elements=%d",sum );
  
   return 0;
